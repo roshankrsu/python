bedd3a6f965eb1b196fa22caea15b78e240b279c
Roshan <roshankrsu@gmail.com>
2026-05-15 09:56:53 +0530

    async python

---
+++ async_python/02_async_two.py
@@ -0,0 +1,17 @@
+import asyncio
+import time
+async def brew(name):
+    print(f"Brewing {name}...")
+    await asyncio.sleep(2)
+    # time.sleep(3)
+    print(f" {name} is ready...")
+
+
+async def main():
+    await asyncio.gather(
+        brew("Masala Chai"),
+        brew("Green Chai"),
+        brew("Ginger Chai"),
+    )
+
+asyncio.run(main())
